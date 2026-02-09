4e8a5d11d160c7997a1a1e566a49f3a8355a4332
Manuel-IA <manuelpc1720@gmail.com>
2026-02-08 21:34:12 -0500

    Added more validations for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,16 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid do |e|
     render json: { error: e.record.errors.full_messages }, status: :unprocessable_entity
   end
+
+  rescue_from CustomerService::NotFound do |e|
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
+
+  rescue_from CustomerService::Unavailable do |e|
+    render json: { error: e.message }, status: :service_unavailable
+  end
+
+  rescue_from Orders::Publishers::PublishFailed do |e|
+    render json: { error: e.message }, status: :service_unavailable
+  end
 end
